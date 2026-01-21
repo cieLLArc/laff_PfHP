c41cad22196c6fa4332e16003687eb83b99b978e
ciellarc <mintsteamz@gmail.com>
2026-01-21 15:08:47 +0800

    finished all homework of 4 weeks

---
+++ Week1/C/Gemv_I_Dots.c
@@ -0,0 +1,11 @@
+#define alpha(i, j) A[(j) * ldA + i] // map alpha( i,j ) to array A
+#define chi(i) x[(i) * incx]         // map chi( i )  to array x
+#define psi(i) y[(i) * incy]         // map psi( i )  to array y
+
+void Dots(int n, double *x, int incx, double *y, int incy, double *gamma);
+
+void MyGemv(int m, int n, double *A, int ldA, double *x, int incx, double *y, int incy)
+{
+  for (int i = 0; i < m; i++)
+    Dots(n, &alpha(i, 0), ldA, x, incx, &psi(i));
+}
